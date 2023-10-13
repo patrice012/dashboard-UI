01c971a983e850cf06cfe175397d044288ca629e
patrice <patricedev02@gmail.com>
2023-10-13 14:50:55 +0000

    update json db

--- server/endpoint.jsx
+++ server/endpoint.jsx
@@ -1,10 +1,10 @@
-const userEndpoint = "http://localhost:3000/users";
-const callEndpoint = "http://localhost:3000/calls";
+// const userEndpoint = "http://localhost:3000/users";
+// const callEndpoint = "http://localhost:3000/calls";
 
 
 
-// const userEndpoint = "https://my-json-server.typicode.com/patrice012/dashboard-UI/users";
-// const callEndpoint =
-//   "https://my-json-server.typicode.com/patrice012/dashboard-UI/calls";
+const userEndpoint = "https://my-json-server.typicode.com/patrice012/dashboard-UI/users";
+const callEndpoint =
+  "https://my-json-server.typicode.com/patrice012/dashboard-UI/calls";
 
 export { userEndpoint, callEndpoint };
